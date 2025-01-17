819925d9d0712c2a2dfc70d549cd79e2cb7d0f88
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-01-17 13:42:59 +0200

    added type hints

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from sympy import isprime
 
 print('This program finds all the prime factors of a number!')
 
-prompt = 'Enter a number: '
-user_input = input(prompt)
+prompt: str = 'Enter a number: '
+user_input: str = input(prompt)
 
 if user_input.isnumeric():
-    number = int(user_input)
+    number: int = int(user_input)
     while number <= 0:
         user_input = input(prompt)
         if user_input.isnumeric():
@@ -17,7 +17,7 @@ if user_input.isnumeric():
 while not user_input.isnumeric():
     user_input = input(prompt)
     if user_input.isnumeric():
-        number = int(user_input)
+        number: int = int(user_input)
         while number <= 0:
             user_input = input(prompt)
             if user_input.isnumeric():
@@ -28,7 +28,7 @@ while not user_input.isnumeric():
 prime_factors = [i for i in range(number) if isprime(i) and number % i == 0]
 
 if prime_factors != []:
-    output = f'The prime factors of {number} are '
+    output: str = f'The prime factors of {number} are '
     for prime_factor in prime_factors:
         if prime_factors.index(prime_factor) != len(prime_factors)-1:
             output += (str(prime_factor) + ', ')
