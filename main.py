0cb6de9e28fd581a708a6994d55b0e292ecd33d8
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-02-03 17:57:35 +0200

    added main guard with some refactoring

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 from sympy import isprime
 from sys import exit
 
-print('Welcome to The Prime Factorizer!')
 
-while True:
-    user_input: str = input('Enter a number: ')
+def main() -> None:
+    print('Welcome to The Prime Factorizer!')
 
-    if user_input == 'exit':
-        print('Thanks for trying my program!')
-        exit()
+    while True:
+        user_input: str = input('Enter a number: ')
 
-    if not user_input.isnumeric():
-        print('Enter a whole number!')
-        continue
+        if user_input == 'exit':
+            print('Thanks for trying my program!')
+            exit()
 
-    else:
-        number: int = int(user_input)
-        prime_factors = [i for i in range(number+1) if isprime(i) and number % i == 0]
-        if prime_factors != [] and len(prime_factors) > 1:
-            output: str = f'The prime factors of {number} are '
-            for prime_factor in prime_factors:
-                if prime_factors.index(prime_factor) != len(prime_factors)-1:
-                    output += (str(prime_factor) + ', ')
-                else:
-                    output += (str(prime_factor) + '.')
-            print(output)
-        elif len(prime_factors) == 1:
-            print(f'The only prime factor of {number} is {prime_factors[-1]}.')
+        if not user_input.isnumeric():
+            print('Enter a whole number!')
+            continue
+
         else:
-            print(f'{number} has no prime factors!')
+            number: int = int(user_input)
+            prime_factors = [i for i in range(
+                number+1) if isprime(i) and number % i == 0]
+            if prime_factors != [] and len(prime_factors) > 1:
+                output: str = f'The prime factors of {number} are '
+                for prime_factor in prime_factors:
+                    if prime_factors.index(prime_factor) != len(prime_factors)-1:
+                        output += (str(prime_factor) + ', ')
+                    else:
+                        output += (str(prime_factor) + '.')
+                print(output)
+            elif len(prime_factors) == 1:
+                print(f'The only prime factor of {
+                      number} is {prime_factors[-1]}.')
+            else:
+                print(f'{number} has no prime factors!')
+
+
+if __name__ == '__main__':
+    main()
